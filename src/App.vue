02225a56669f200b6177be1aa60ff0e5e8c75cb0
Melanie Bousquet <bousquet.melanie@gmail.com>
2017-12-08 16:54:23 +0100

    carousel + carouselSlide

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <carousel>
+      <carousel-slide v-for="n in slides" :index="n - 1">
+        <div style="position:absolute; left:0; right: 0; text-align:center; top: 50%; color: #FFF; font-size:16px;">Slide {{ n }}</div>
+        <img :src="'http://lorempicsum.com/futurama/630/300/' + n" width="100%">
+      </carousel-slide>
+    </carousel>
+    <button @click="addSlide">Ajouter un slide</button>
+    <button @click="removeSlide">Supprimer un slide</button>
+  </div>
+</template>
+
+<script>
+import Carousel from './components/carousel/Carousel'
+import CarouselSlide from './components/carousel/CarouselSlide'
+
+export default {
+  data () {
+    return {
+      slides: 5,
+    }
+  },
+  methods: {
+    addSlide () {
+      this.slides++
+    },
+    removeSlide () {
+      this.slides--
+    }
+  },
+  components: {
+    Carousel,
+    CarouselSlide
+  }
+}
+</script>
+
